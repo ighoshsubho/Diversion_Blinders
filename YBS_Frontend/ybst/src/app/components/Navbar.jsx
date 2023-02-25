047fe1d8e84e5c79992d09da23f7471f639f0a84
Aditya007Datta <adityadattaofficial@gmail.com>
2023-02-25 13:01:11 +0530

    hero commit

--- YBS_Frontend/ybst/src/app/components/Navbar.jsx
+++ YBS_Frontend/ybst/src/app/components/Navbar.jsx
@@ -1,16 +1,53 @@
-import React from 'react'
+"use client";
 
-const Navbar = () => {
+import Link from "next/link";
+
+import classNames from "classnames";
+
+import Image from "next/image";
+import { Container } from "./container";
+import { Button } from "./Button";
+
+export const Navbar = () => {
   return (
-    <div className="navbar navbar-glass">
-	<div className="navbar-start">
-		<a className="navbar-item">Ripple UI</a>
-	</div>
-	<div className="navbar-end">
-		<a className="navbar-item">Home</a>
-	</div>
-</div>
-  )
-}
+    <header className="fixed top-0 left-0 z-10 w-full border-b border-transparent-white backdrop-blur-[12px]">
+      <Container className="flex h-navigation-height">
+        <Link className="flex items-center text-md" href="/">
+          <Image src={Logo} alt="logo" height={60} width={60} />Y B S
+        </Link>
 
-export default Navbar
+        <div className="ml-auto flex h-full items-center">
+          <div className={classNames("transition-[visibility] md:visible")}>
+            <nav
+              className={classNames(
+                "fixed top-navigation-height left-0 h-[calc(100vh_-_var(--navigation-height))] w-full overflow-auto bg-background transition-opacity duration-500 md:relative md:top-0 md:block md:h-auto md:w-auto md:translate-x-0 md:overflow-hidden md:bg-transparent md:opacity-100 md:transition-none"
+              )}
+            >
+              <ul
+                className={classNames(
+                  "flex h-full flex-col md:flex-row md:items-center [&_li]:ml-6 [&_li]:border-b [&_li]:border-grey-dark md:[&_li]:border-none",
+                  "ease-in [&_a:hover]:text-grey [&_a]:flex [&_a]:h-10 [&_a]:w-full [&_a]:translate-y-8 [&_a]:items-center [&_a]:text-lg [&_a]:transition-[color,transform] [&_a]:duration-300 md:[&_a]:translate-y-0 md:[&_a]:text-sm [&_a]:md:transition-colors"
+                )}
+              >
+                <li>
+                  <Link href="/about">About Us</Link>
+                </li>
+                <li>
+                  <Link href="/image">Services</Link>
+                </li>
+                <li>
+                  <Link href="/login">Log in</Link>
+                </li>
+                <li>
+                  <Button href="/register">
+                    <span>Register </span>
+                  </Button>
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </div>
+      </Container>
+    </header>
+  );
+};
