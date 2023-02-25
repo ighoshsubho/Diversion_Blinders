195c46ffa30d502c05fe0a0761d22f02a5875d6a
Aditya007Datta <adityadattaofficial@gmail.com>
2023-02-25 12:24:07 +0530

    hero commit

---
+++ YBS_Frontend/ybst/src/app/components/Hero/herotitle.jsx
@@ -0,0 +1,31 @@
+import classNames from "classnames";
+
+export const HeroTitle = ({ children, className }) => {
+  return (
+    <h1
+      className={classNames(
+        "text-gradient font-[Gajraj One] my-6 text-6xl md:text-8xl",
+        className
+      )}
+    >
+      {children}
+    </h1>
+  );
+};
+
+export const HeroSubtitle = ({ children, className }) => {
+  return (
+    <p
+      className={classNames(
+        "mb-12 text-lg text-primary-text md:text-xl",
+        className
+      )}
+    >
+      {children}
+    </p>
+  );
+};
+
+export const Hero = ({ children }) => {
+  return <div className="text-center">{children}</div>;
+};
