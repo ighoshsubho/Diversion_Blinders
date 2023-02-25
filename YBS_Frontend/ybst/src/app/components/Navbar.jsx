6c75ee8f3458b97863620eb5dde341bd74e03d18
jyotiradityaroy <jyotiraditya.roy.aiml25@heritageit.edu.in>
2023-02-25 11:52:06 +0530

    login commit

---
+++ YBS_Frontend/ybst/src/app/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className="navbar navbar-glass">
+	<div className="navbar-start">
+		<a className="navbar-item">Ripple UI</a>
+	</div>
+	<div className="navbar-end">
+		<a className="navbar-item">Home</a>
+	</div>
+</div>
+  )
+}
+
+export default Navbar
--- YBS_Frontend/ybst/src/app/layout.js
+++ YBS_Frontend/ybst/src/app/layout.js
@@ -1,3 +1,4 @@
+import Navbar from './components/Navbar'
 import './globals.css'
 
 export const metadata = {
@@ -8,7 +9,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <Navbar/>
+        {children}</body>
     </html>
   )
 }
